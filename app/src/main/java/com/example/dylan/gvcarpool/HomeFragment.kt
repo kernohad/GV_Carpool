f3439452dabf984f796c98ca8af22abbbd9939f8
Dylan Kernohan <kernohad@mail.gvsu.edu>
2018-10-28 14:54:29 -0400

    Created the three main screen fragments (Home, Profile, Radar)
    created the fragments, updated the stock navigation bar, made the navigation bar load the correct fragment.

---
+++ app/src/main/java/com/example/dylan/gvcarpool/HomeFragment.kt
@@ -0,0 +1,36 @@
+package com.example.dylan.gvcarpool
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+}
