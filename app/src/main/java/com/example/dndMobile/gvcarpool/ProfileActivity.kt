179c8999cd6a8d12d42fd959ae2c9a4cf07ad220
Dylan Kernohan <kernohad@mail.gvsu.edu>
2018-11-22 16:44:35 -0500

    Can send data (users name) to profile activity. Strange bug though
    When I try to change the text in the profile fragment, it just writes over the text, it does not change it. Maybe multiple instances of the fragment are being made? Not sure.

--- app/src/main/java/com/example/dndMobile/gvcarpool/ProfileActivity.kt
+++ app/src/main/java/com/example/dndMobile/gvcarpool/ProfileActivity.kt
@@ -2,6 +2,7 @@ package com.example.dndMobile.gvcarpool
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -9,11 +10,18 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
+        // Get the extra (DB data) from the intent
+        val extras = intent.extras ?: return
+        val name = extras.getString("data")
+
+        // Create a new intance of the profile fragment and pass it the data
+        //TODO: Right now we only pass it the name as the data. Make this the entire DB object
+        val profileFragment = ProfileFragment.newInstance(name)
+
         // Set the fragment to be profile fragment
         supportFragmentManager.beginTransaction()
-                .replace(R.id.profileFragmentContainer, ProfileFragment())
+                .replace(R.id.profileFragmentContainer, profileFragment)
                 .commit()
 
-        //TODO: Use DB entry from intent and create a profileFragment object and pass it the info to set the fields. Or something
     }
 }
--- app/src/main/java/com/example/dndMobile/gvcarpool/ProfileFragment.kt
+++ app/src/main/java/com/example/dndMobile/gvcarpool/ProfileFragment.kt
@@ -11,29 +11,52 @@ import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 
 class ProfileFragment : Fragment() {
-
+    private var root: View? = null   // create a global variable which will hold your layout
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val viewOfLayout = inflater.inflate(R.layout.fragment_profile, container, false)
-
+        root = inflater.inflate(R.layout.fragment_profile, container, false)
 
 
         //TODO: Find out of profile belongs to current user or not
         //      Allow edits to profile if so
 
-        //TODO: Pull profile info from db
+        //TODO: Pull profile info from db and populate fields
         //      :Name
         //      :bio
         //      :photo, if exists.  if not, use default
         //      :common routes
+        // EXAMPLE of pull info from data passed to fragment and populating field. DB entry will be stored as argument to fragment
 
         //TODO: Stop focus when clicked off of bio edit text
 
         //TODO: Figure out if we want common departures/arrivals to be decided by actual data or user specified
         
-        return viewOfLayout
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (arguments != null) {
+            val name = arguments?.getString("data")
+            root!!.nameTextView.text = name
+        }
+    }
+
+    // This is used when a new instance of the profile fragment is made for the Profile Activity so we can send data and populate all the fields.
+    // Reference http://chirag-limbachiya.blogspot.com/2017/08/pass-parameters-from-activity-to.html Use this for example of different types.
+    companion object {
+
+        // TODO: make data be of type database entry so we can get all info, not just name
+        fun newInstance(data: String?): ProfileFragment {
+
+            val args = Bundle()
+            args.putString("data", data)
+            val fragment = ProfileFragment()
+            fragment.arguments = args
+            return fragment
+        }
     }
 
 
