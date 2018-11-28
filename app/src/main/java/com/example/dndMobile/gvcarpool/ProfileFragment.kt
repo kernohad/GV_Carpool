1f1588668ca85282f8ca6f235830a0a4c93bc4d8
Dustin Thurston <dthurston1996@gmail.com>
2018-11-28 10:36:35 -0500

    Added additional button functionality
    Right now, it just sets the visibility of the other button and enables/disables the editText.  Need to add firebase stuff now.

--- app/src/main/java/com/example/dndMobile/gvcarpool/ProfileFragment.kt
+++ app/src/main/java/com/example/dndMobile/gvcarpool/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.dndMobile.gvcarpool
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -17,8 +18,12 @@ class ProfileFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val viewOfLayout = inflater.inflate(R.layout.fragment_profile, container, false)
+        
+        return viewOfLayout
+    }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         //TODO: Find out of profile belongs to current user or not
         //      Allow edits to profile if so
@@ -32,9 +37,36 @@ class ProfileFragment : Fragment() {
         //TODO: Stop focus when clicked off of bio edit text
 
         //TODO: Figure out if we want common departures/arrivals to be decided by actual data or user specified
-        
-        return viewOfLayout
+
+        //Is the user editing their profile or nah?
+        var editMode = false
+
+        editButton.setOnClickListener{_ ->
+            editMode = true
+
+            aboutEditText.isEnabled = true
+
+            saveButton.visibility = View.VISIBLE
+            editButton.visibility = View.INVISIBLE
+
+        }
+
+        profilePicture.setOnClickListener{_ ->
+            //TODO: image select/take photo intent
+
+        }
+
+        saveButton.setOnClickListener{_ ->
+            //TODO: Store changes in firebase
+            aboutEditText.isEnabled = false
+
+            saveButton.visibility = View.INVISIBLE
+            editButton.visibility = View.INVISIBLE
+
+        }
     }
+
+
 
 
 }
