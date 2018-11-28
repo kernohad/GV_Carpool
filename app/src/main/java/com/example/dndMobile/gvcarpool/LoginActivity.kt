b14178e61af75c787c25c5ece88cac5f2766bac9
Dylan Kernohan <kernohad@mail.gvsu.edu>
2018-11-27 23:36:14 -0500

    Sign in and Logout work.

--- app/src/main/java/com/example/dndMobile/gvcarpool/LoginActivity.kt
+++ app/src/main/java/com/example/dndMobile/gvcarpool/LoginActivity.kt
@@ -3,18 +3,28 @@ package com.example.dndMobile.gvcarpool
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.text.TextUtils
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_feed_detail.*
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlin.math.sign
 
 class LoginActivity : AppCompatActivity() {
+    // Reference to firebase authenticator
+    private var auth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        // Initialize firebase reference
+        auth = FirebaseAuth.getInstance()
+
         loginButton.setOnClickListener{ _ ->
             // TODO: Make and call firebase login function. For now, just launch main activity
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            signIn()
         }
 
         registerLabel.setOnClickListener { _ ->
@@ -22,5 +32,41 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun signIn(){
+        val email = emailField.text.toString()
+        val password = passwordField.text.toString()
+
+        if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
+            auth!!.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this){ task ->
+                        if(task.isSuccessful){
+                            //Sign in success. Start app
+                            val currentUser: FirebaseUser = auth?.currentUser!!
+                            launchApp(currentUser)
+                        }else{
+                            Snackbar.make(loginRoot, "Authentication Failed", Snackbar.LENGTH_SHORT).show()
+                        }
+                    }
+        }else{
+            Snackbar.make(loginRoot, "Please fill out both fields.", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly
+        if(auth?.currentUser != null) {
+            val currentUser: FirebaseUser = auth?.currentUser!!
+            launchApp(currentUser)
+        }
+    }
+
+    private fun launchApp(user: FirebaseUser){
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("user", user)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/dndMobile/gvcarpool/MainActivity.kt
+++ app/src/main/java/com/example/dndMobile/gvcarpool/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.dndMobile.gvcarpool
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), HomeFragment.OnFragmentInteractionListener {
+    // Reference to firebase authenticator
+    private var auth: FirebaseAuth? = null
+
     override fun onFragmentInteraction(uri: Uri) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
@@ -35,8 +42,37 @@ class MainActivity : AppCompatActivity(), HomeFragment.OnFragmentInteractionList
         false
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.logout_button -> {
+                // Logout of firebase
+                auth!!.signOut()
+
+                // Make intent back to login activity
+                val intent = Intent(this, LoginActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Initialize firebase reference
+        auth = FirebaseAuth.getInstance()
+
         setContentView(R.layout.activity_main)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         navigation.selectedItemId = R.id.navigation_home
