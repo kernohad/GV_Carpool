128c259223d6a8b34e18c5f5e340df39614783f3
Dylan Kernohan <kernohad@mail.gvsu.edu>
2018-12-03 01:01:05 -0500

    Finished new carpool screen with DB support
    Can now create a new Ride and have it store to the DB. We store each ride with a unique ID (date/time it was made) and the user Id that made it along with all the ride info.

--- app/src/main/java/com/example/dndMobile/gvcarpool/NewCarpoolActivity.kt
+++ app/src/main/java/com/example/dndMobile/gvcarpool/NewCarpoolActivity.kt
@@ -3,23 +3,45 @@ package com.example.dndMobile.gvcarpool
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType.TYPE_NULL
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_new_carpool.*
 import com.kunzisoft.switchdatetime.SwitchDateTimeDialogFragment
-import java.text.DateFormat.getDateTimeInstance
 import java.text.SimpleDateFormat
-import java.time.LocalDateTime
 import java.util.*
 
 
 class NewCarpoolActivity : AppCompatActivity() {
+    // Reference to firebase authenticator and DB
+    private var auth: FirebaseAuth? = null
+    private var databaseReference: DatabaseReference? = null
+    private var type = "offer"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_carpool)
 
+        // Set type based on default value (offer)
+        toggleType()
 
+        // Set click listners to toggle type
+        offerButton.setOnClickListener { _ ->
+            type = "offer"
+            toggleType()
+        }
 
+        requestButton.setOnClickListener { _ ->
+            type = "request"
+            toggleType()
+        }
 
+        // Initialize firebase reference
+        auth = FirebaseAuth.getInstance()
+        databaseReference = FirebaseDatabase.getInstance().reference.child("Rides")
+
+        // Focus Listener for 'When' Field. It starts pulls up the Date Time picker
         // Reference to Librfary Used: https://github.com/Kunzisoft/Android-SwitchDateTimePicker
         whenField.setOnFocusChangeListener { _ , hasFocus ->
             if(hasFocus){
@@ -67,5 +89,46 @@ class NewCarpoolActivity : AppCompatActivity() {
                 dateTimeDialogFragment.show(supportFragmentManager, "dialog_time")
             }
         }
+
+        submitButton.setOnClickListener{ _ ->
+
+            // TODO: Field Validation. Make sure required fields are not empty.
+
+            // Set DB reference specific ride and save data
+            val rideDb = databaseReference!!.child(Date().toString())        // Get Current Date time and use as Ride ID
+            rideDb.child("user").setValue(auth?.currentUser!!.uid)
+            rideDb.child("type").setValue(type)
+            rideDb.child("to").setValue(toField.text.toString())
+            rideDb.child("from").setValue(fromField.text.toString())
+            rideDb.child("when").setValue(whenField.text.toString())
+            rideDb.child("total_seats").setValue(totalSeatsField.text.toString())
+            rideDb.child("gas_money").setValue(gasMoneyField.text.toString())
+            rideDb.child("seats_available").setValue(totalSeatsField.text.toString())
+            rideDb.child("notes").setValue(notesField.text.toString())
+
+            // Display message and finish activity
+            Toast.makeText(this,
+                    "New $type was created.",
+                    Toast.LENGTH_SHORT).show()
+            finish()
+
+        }
+
+        cancelButton.setOnClickListener { _ ->
+            // Finish the activity and return to previous activity (Home)
+            finish()
+        }
+    }
+
+    private fun toggleType(){
+        // Set type colors
+        if(type == "offer"){
+            offerButton.setBackgroundResource(R.drawable.offer_background)
+            requestButton.setBackgroundResource(R.drawable.neutral_background)
+        }else if(type == "request"){
+            offerButton.setBackgroundResource(R.drawable.neutral_background)
+            requestButton.setBackgroundResource(R.drawable.request_background)
+        }
     }
 }
+
