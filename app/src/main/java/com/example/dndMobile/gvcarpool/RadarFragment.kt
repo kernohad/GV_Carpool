a1536345862db63dd72960a1abe25d597157095e
Dustin Thurston <dthurston1996@gmail.com>
2018-12-09 01:49:34 -0500

    Now asks permission for location and displays user location on map

--- app/src/main/java/com/example/dndMobile/gvcarpool/RadarFragment.kt
+++ app/src/main/java/com/example/dndMobile/gvcarpool/RadarFragment.kt
@@ -1,10 +1,15 @@
 package com.example.dndMobile.gvcarpool
 
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -19,6 +24,8 @@ class RadarFragment : Fragment(), OnMapReadyCallback {
 
     //The map
     private lateinit var gMap: GoogleMap
+    private lateinit var lastLocation: Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -27,6 +34,7 @@ class RadarFragment : Fragment(), OnMapReadyCallback {
 
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this.activity!!)
 
         return root
     }
@@ -34,15 +42,43 @@ class RadarFragment : Fragment(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         gMap = googleMap
 
+        //TODO: replace allendale with device's current location.
         val allendale = LatLng(42.9639, -85.8889)
         gMap.addMarker(MarkerOptions().position(allendale).title("Marker in Allendale"))
         gMap.moveCamera(CameraUpdateFactory.newLatLng(allendale))
+        gMap.animateCamera(CameraUpdateFactory.zoomIn())
+        gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(allendale, 15.0f))
+
+        setUpMap()
+    }
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this.context!!,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this.activity!!,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+
+        gMap.isMyLocationEnabled = true
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+
+            if(location != null){
+                lastLocation = location
+                val currLatLon = LatLng(location.latitude, location.longitude)
+                gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currLatLon, 12f))
+            }
+        }
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
 
-
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
     }
 
 }
